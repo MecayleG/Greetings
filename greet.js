04c58c404242335900f84c3348ea4d06ed713a3a
root <root@DESKTOP-V8P4AHJ.localdomain>
2020-05-25 17:24:12 +0200

    added functionality and styling

---
+++ greet.js
@@ -0,0 +1,47 @@
+const greetFunction = TheGreetFunction();
+const greetButton = document.querySelector(".greetBtn");
+const nameString = document.querySelector(".nameString");
+const msg = document.getElementById("theGreet");
+const count = document.getElementById("theCount");
+
+// count.innerHTML = greetFunction.getLength();
+
+
+
+
+// var counter = 0;
+// count.innerHTML = counter;
+
+function myFunction(){
+	 var valSelected =  document.querySelector("input[name='lang']:checked").value;
+	 msg.innerHTML = greetFunction.langButton(valSelected, nameString.value);
+	 greetFunction.namesStored(nameString.value);
+	 count.innerHTML = greetFunction.getLength();
+	 nameString.value = "";
+	 
+	// check diagrams of how dom and local storage
+	 
+
+	//  if(greetString.value !== ""){
+	// 	counter ++;
+	//  var valSelected =  document.querySelector("input[name='lang']:checked").value;
+	//  msg.innerHTML = valSelected
+	// if(valSelected === "English"){
+	// 	msg.innerHTML = "Hello, " + greetString.value;
+	// }
+	// if(valSelected === "Espanol"){
+	// 	msg.innerHTML = "Hola, " + greetString.value;
+	// }
+	// if(valSelected === "Japanese"){
+	// 	msg.innerHTML = "Kon'nichiwa, " + greetString.value;
+	// }
+	// };
+	//  greetString.value = "";
+	//  // count how many times button is pressed
+	//  count.innerHTML = counter;
+}
+function deleteItems(){
+	localStorage.clear();
+}
+greetButton.addEventListener("click", myFunction);
+	
