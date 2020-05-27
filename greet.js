4de3c9ff63c411c74cb181f01e924d4c5805b77f
root <root@DESKTOP-V8P4AHJ.localdomain>
2020-05-27 22:17:03 +0200

    moved localStorage and updated factory

--- greet.js
+++ greet.js
@@ -3,43 +3,30 @@ const greetButton = document.querySelector(".greetBtn");
 const nameString = document.querySelector(".nameString");
 const msg = document.getElementById("theGreet");
 const count = document.getElementById("theCount");
-
-// count.innerHTML = greetFunction.getLength();
-
+let arrayNames;
 
 
 
-// var counter = 0;
-// count.innerHTML = counter;
 
 function myFunction(){
+	var inputVal = nameString.value;
+	namesStored(inputVal.toLowerCase());
 	 var valSelected =  document.querySelector("input[name='lang']:checked").value;
-	 msg.innerHTML = greetFunction.langButton(valSelected, nameString.value);
-	 greetFunction.namesStored(nameString.value);
-	 count.innerHTML = greetFunction.getLength();
+	 msg.innerHTML = greetFunction.langButton(valSelected, inputVal);
+	 count.innerHTML = JSON.parse(localStorage.getItem("names")).length;
+
 	 nameString.value = "";
 	 
-	// check diagrams of how dom and local storage
 	 
-
-	//  if(greetString.value !== ""){
-	// 	counter ++;
-	//  var valSelected =  document.querySelector("input[name='lang']:checked").value;
-	//  msg.innerHTML = valSelected
-	// if(valSelected === "English"){
-	// 	msg.innerHTML = "Hello, " + greetString.value;
-	// }
-	// if(valSelected === "Espanol"){
-	// 	msg.innerHTML = "Hola, " + greetString.value;
-	// }
-	// if(valSelected === "Japanese"){
-	// 	msg.innerHTML = "Kon'nichiwa, " + greetString.value;
-	// }
-	// };
-	//  greetString.value = "";
-	//  // count how many times button is pressed
-	//  count.innerHTML = counter;
+	
 }
+function namesStored(nameEntered){
+		arrayNames = JSON.parse(localStorage.getItem("names")) || [];
+		if(!arrayNames.includes(nameEntered) && nameEntered !== ""){
+			arrayNames.push(nameEntered)
+			localStorage.setItem("names", JSON.stringify(arrayNames))
+		}
+	} 
 function deleteItems(){
 	localStorage.clear();
 	count.innerHTML = 0;
